4593a6f7048e0aaa0dec696efc3f5e50ed530cf4
luis isturiz <Aliatore>
2021-07-28 00:34:52 -0400

    correcciones al bug en pronosticos

--- src/screens/Stacks/ForecastsScreen.js
+++ src/screens/Stacks/ForecastsScreen.js
@@ -216,9 +216,9 @@ const ForecastsScreen = ({navigation}) => {
                 {/* <Text  style={styles.top_text}>Pronóstico del día</Text> */}
               </View>
               <View style={styles.bot}>
-              {data.d_1 ==  null || data.d_1 == undefined || data.d_1 == "" &&
-              data.d_3 ==  null || data.d_3 == undefined ||  data.d_3 == "" &&
-              data.d_4 ==  null || data.d_4 == undefined ||  data.d_4 == "" &&
+              {data.d_1 ==  null || data.d_1 == undefined || data.d_1 == "" ||
+              data.d_3 ==  null || data.d_3 == undefined ||  data.d_3 == "" ||
+              data.d_4 ==  null || data.d_4 == undefined ||  data.d_4 == "" ||
               data.d_2 == null || data.d_2 == undefined ||  data.d_2 == "" ?
           
                 (
@@ -236,31 +236,6 @@ const ForecastsScreen = ({navigation}) => {
                             </View>
                             </Card.Content>
                         </Card>
-                  /* <>
-                  {data.d_1.simples ==  null || data.d_1.simples == undefined || data.d_1.simples == [] || data.d_1.simples == "" &&
-                    data.d_1.combinadas ==  null || data.d_1.combinadas == undefined || data.d_1.combinadas == [] || data.d_1.combinadas == "" &&
-                    data.d_3.simples ==  null || data.d_3.simples == undefined || data.d_3.simples == [] || data.d_3.simples == "" &&
-                    data.d_3.combinadas ==  null || data.d_3.combinadas == undefined || data.d_3.combinadas == [] || data.d_3.combinadas == "" &&
-                    data.d_4.simples ==  null || data.d_4.simples == undefined || data.d_4.simples == [] || data.d_4.simples == "" &&
-                    data.d_4.combinadas ==  null || data.d_4.combinadas == undefined || data.d_4.combinadas == [] || data.d_4.combinadas == "" &&
-                    data.d_2 == null || data.d_2 == undefined || data.d_2 == [] || data.d_2 == "" ?
-                        (<Card style={[styles.card, {marginTop: 20}]}>
-                          <Card.Content>
-                            <View style={styles.colapsed_body}>
-                              <View style={styles.container_body}>
-                                <View style={{width:'100%'}}>
-                                  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                                    <Text style={styles.top_text_empty}>Nuestros pronosticadores están trabajando... </Text>
-                                    <Text style={styles.top_text_empty}>Pronto tendrás tus pronósticos, recibirás una notificación en tu correo electrónico.</Text>
-                                  </View>
-                                </View>
-                              </View>
-                            </View>
-                            </Card.Content>
-                        </Card>)
-                        :null
-                  }
-                  </> */
                 )
                 :
                 null
@@ -268,7 +243,8 @@ const ForecastsScreen = ({navigation}) => {
               {data.d_1 !== null && data.d_1 !== undefined && data.d_1 !== "" ?
               (
                 <>
-                {data.d_1.simples == null || data.d_1.simples == undefined || data.d_1.simples == "" ? 
+                {data.d_1.simples == null || data.d_1.simples == undefined || data.d_1.simples == "" ||
+                data.d_1.combinadas == "" || data.d_1.combinadas == "" || data.d_1.combinadas == "" ? 
                   (<Text  style={styles.top_text}> </Text>) 
                   : 
                   (<Text  style={styles.top_text}>Plan General</Text>)
@@ -333,7 +309,8 @@ const ForecastsScreen = ({navigation}) => {
               {data.d_3 !== null && data.d_3 !== undefined && data.d_3 !== "" ?
               (
                 <>
-                {data.d_3.simples == null || data.d_3.simples == undefined || data.d_3.simples == "" ? 
+                {data.d_3.simples == null || data.d_3.simples == undefined || data.d_3.simples == "" ||
+                data.d_3.combinadas == "" || data.d_3.combinadas == "" || data.d_3.combinadas == "" ? 
                   (<Text  style={styles.top_text}> </Text>) 
                   : 
                   (<Text  style={styles.top_text}>Plan Sports</Text>)
@@ -428,7 +405,8 @@ const ForecastsScreen = ({navigation}) => {
               {data.d_4 !== null && data.d_4 !== undefined && data.d_4 !== "" ?
               (
                 <>
-                {data.d_4.simples == null || data.d_4.simples == undefined || data.d_4.simples == "" ? 
+                {data.d_4.simples == null || data.d_4.simples == undefined || data.d_4.simples == "" ||
+                data.d_4.combinadas == "" || data.d_4.combinadas == "" || data.d_4.combinadas == "" ? 
                   (<Text  style={styles.top_text}> </Text>) 
                   : 
                   (<Text  style={styles.top_text}>Plan Trial</Text>)
